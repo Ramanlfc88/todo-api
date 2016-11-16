cf579c1d05a23b495d410fa97e6057152b98588c
Raman <ramanlfc@gmail.com>
2016-11-16 14:10:28 +0530

    setup todos, and a todos route

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id: 1,
+    description: "Meet John at Lunch",
+    completed: false
+}, {
+        id: 2,
+        description: "Go to Market",
+        completed: false
+}];
 
-app.get('/',function (req, res) {
+
+app.get('/', function (req, res) {
     res.send('welcome to the todo page');
+});
+
+app.get('/todos', function (req, res) {
+    res.json(todos);
 });
 
 
 app.listen(PORT, function () {
-console.log('server started at port:' +PORT);
+    console.log('server started at port:' + PORT);
 });
