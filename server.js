07e7476e6a82142a70a1a82124df6e0e16eb37c8
Raman <ramanlfc@gmail.com>
2016-11-16 14:32:51 +0530

    added todos/id route

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -9,8 +11,11 @@ var todos = [{
         id: 2,
         description: "Go to Market",
         completed: false
-}];
-
+    }, {
+        id: 3,
+        description: "Finish breakfast",
+        completed: true
+    }];
 
 app.get('/', function (req, res) {
     res.send('welcome to the todo page');
@@ -20,6 +25,21 @@ app.get('/todos', function (req, res) {
     res.json(todos);
 });
 
+app.get('/todos/:id', function (req, res) {
+    var id = parseInt(req.params.id);
+    var todo;
+
+    for (let i = 0; i < todos.length; i++) {
+        if (id === todos[i].id) {
+            todo = todos[i];
+        }
+    }// end for
+    
+    if (todo)
+        res.json(todo);
+    else
+        res.status(400).send();
+});
 
 app.listen(PORT, function () {
     console.log('server started at port:' + PORT);
