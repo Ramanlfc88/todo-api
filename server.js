8432df319ba8b20636f842f94e93b8ab887373fa
Raman <ramanlfc@gmail.com>
2016-11-23 07:24:23 +0530

    get todos now use db

--- server.js
+++ server.js
@@ -29,25 +29,55 @@ app.get('/', function (req, res) {
 
 app.get('/todos', function (req, res) {
     var queryParams = req.query;
-
+    var where = {};
     if (queryParams.completed === undefined && queryParams.desc === undefined) {
-        return res.json(todos);
     }
-    var filteredTodos = todos;
+
     var completed, description;
     if (queryParams.completed !== undefined) {
         completed = queryParams.completed === 'true' ? true : false;
-        filteredTodos = _.where(filteredTodos, { completed: completed });
+        where.completed = completed;
     }
 
     if (queryParams.desc !== undefined) {
         description = queryParams.desc.toLowerCase();
-        filteredTodos = _.filter(filteredTodos, function (item) {
-            return item.description.toLowerCase().indexOf(description) > -1;
-        });
+        where.description = {
+            $like : '%'+ description+ '%'
+        };
     }
 
-    res.json(filteredTodos);
+    db.todo.findAll({
+        where: where
+    }).then(function (todos) {
+        if (todos) {
+            res.status(200).json(todos);
+        }
+        else
+            res.status(404).send();
+    }).catch(function (err) {
+        res.status(500).send();
+    });
+    /*
+        if (queryParams.completed === undefined && queryParams.desc === undefined) {
+            return res.json(todos);
+        }
+        var filteredTodos = todos;
+        var completed, description;
+        if (queryParams.completed !== undefined) {
+            completed = queryParams.completed === 'true' ? true : false;
+            filteredTodos = _.where(filteredTodos, { completed: completed });
+        }
+    
+        if (queryParams.desc !== undefined) {
+            description = queryParams.desc.toLowerCase();
+            filteredTodos = _.filter(filteredTodos, function (item) {
+                return item.description.toLowerCase().indexOf(description) > -1;
+            });
+        }
+    
+        res.json(filteredTodos);
+    */
+
 });
 
 app.get('/todos/:id', function (req, res) {
@@ -79,7 +109,7 @@ app.get('/todos/:id', function (req, res) {
 
 app.post('/todos', function (req, res) {
     var body = _.pick(req.body, 'description', 'completed'); // strip away overposted data
-
+/*
     if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
         return res.status(400).send();
     }
@@ -87,9 +117,9 @@ app.post('/todos', function (req, res) {
     var currentId = todos[todos.length - 1].id + 1;
     body.description = body.description.trim();
     body.id = currentId;
-    // todos.push(body);
-    //  res.json(body);
-
+     todos.push(body);
+      res.json(body);
+*/
     db.todo.create(body).then(function (todo) {
         res.status(200).json(todo.toJSON());
     }).catch(function (err) {
